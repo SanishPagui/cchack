d3a5bd0c5b9234d88e9f8826c974c6dea427c781
Sanish <sanishpagui162@gmail.com>
2025-07-12 00:05:44 +0530

    links page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Page() {
           </div>
 
           <div className="flex gap-12 mt-8">
-              <RainbowButton className="group" onClick={() => { window.location.href = "./"; }}>
+              <RainbowButton className="group" onClick={() => { window.location.href = "./links"; }}>
                 <Rocket className="w-5 h-5 mr-2" />
                 Start Exploring
                 <ArrowRight className="w-4 h-4 ml-2" />
