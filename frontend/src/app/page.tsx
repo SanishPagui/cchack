076b1c31cb952ceffce85793c5e27ad4298f28a5
Sanish <sanishpagui162@gmail.com>
2025-07-11 23:07:51 +0530

    Revert "i pushed node module :)"
    This reverts commit 7afc354b5f9f2a5743c130a5d18173d8d2e4ecaf.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
+import EducationLayer from "./components/EducationLayer";
 import { Globe } from "@/components/magicui/globe";
 import { Particles } from "@/components/magicui/particles";
 import { useTheme } from "next-themes";
@@ -39,6 +40,9 @@ export default function Page() {
         />
         <Globe className="absolute left-240 top-1/2 transform -translate-y-1/2 scale-[1.5] z-0" />
         <div className="pointer-events-none absolute inset-0 h-full w-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
+      </div>
+      <div>
+        <EducationLayer/>
       </div>
     </div>
   );
